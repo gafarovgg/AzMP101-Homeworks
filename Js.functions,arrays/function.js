0352acaff9414256feb19f3dbc2a915e98bfd4a2
gafarovgg <mi6whu2j9>
2024-04-16 23:58:20 +0400

    js functions,arrays

---
+++ Js.functions,arrays/function.js
@@ -0,0 +1,57 @@
+// const countries = ["Finland", "Denmark", "Sweden", "Norway", "Iceland"];
+// const updatedcountries = countries.map((item) =>
+//   item.slice(0, 3).toLocaleUpperCase()
+// );
+// console.log(updatedcountries);
+//*****************************************************************************************/
+// const products = [
+//   {
+//     id: 1,
+//     title: "Smartphone",
+//     description: "A high-end smartphone with the latest features.",
+//     price: 799.99,
+//   },
+//   {
+//     id: 2,
+//     title: "Laptop",
+//     description: "Powerful laptop with a large screen and fast processor.",
+//     price: 1299.99,
+//   },
+//   {
+//     id: 3,
+//     title: "Coffee Maker",
+//     description: "An automatic coffee maker with a built-in grinder.",
+//     price: 99.99,
+//   },
+//   {
+//     id: 4,
+//     title: "Headphones",
+//     description: "Wireless over-ear headphones with noise-cancellation.",
+//     price: 199.99,
+//   },
+//   {
+//     id: 5,
+//     title: "Smart TV",
+//     description: "55-inch 4K Smart TV with streaming apps built-in.",
+//     price: 699.99,
+//   },
+// ];
+
+// let sumOfPrices = 0;
+// for (let i = 0; i < products.length; i++) {
+//   sumOfPrices += products[i].price;
+// }
+
+// console.log("CEM:", sumOfPrices);
+// console.log("ORTALAMA:", sumOfPrices / products.length);
+//*****************************************************************************************/
+// function findTwoDigitFactors() {
+//   for (let i = 10; i < 100; i++) {
+//     if (i % 11 === 0) {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// findTwoDigitFactors();
+//*****************************************************************************************/
