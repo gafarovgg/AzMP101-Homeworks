f3c9112008ab179d24843c482272d4c6d9c9b75d
gafarovgg <mi6whu2j9@code.edu.az>
2024-04-18 12:07:30 +0400

    some changes in array

--- 03.js-array-methods/array.js
+++ 03.js-array-methods/array.js
@@ -65,17 +65,13 @@
 //   }
 // });
 // console.log(`t ilə başlayıb t ilə bitənlərin sayı :${count}`);
-//---------------------------------------10----------------------------------------//
-// let totalSum = 0;
-// for (let i = 0; i < arr.length; i++) {
-//   totalSum += arr[i].key;
-// }
-
-// console.log("Keylərin cəmi:", totalSum);
-//---------------------------------------8----------------------------------------//
-
-// let filteredArr = arr.filter((arr) => arr.name.length === 4);
-// console.log(filteredArr);
+//---------------------------------------4----------------------------------------//
+// arr.forEach((obj) => {
+//   if (obj.name.endsWith("e")) {
+//     obj.name = "SuperDev";
+//   }
+// });
+// console.log(arr);
 //---------------------------------------5.6----------------------------------------//
 
 // let longestNameObj = arr[0];
@@ -85,5 +81,29 @@
 //     longestNameObj = arr[i];
 //   }
 // }
-
 // console.log("Ən uzun name və key olan: ", longestNameObj);
+//---------------------------------------8----------------------------------------//
+
+// let filteredArr = arr.filter((arr) => arr.name.length === 4);
+// console.log(filteredArr);
+
+//---------------------------------------10----------------------------------------//
+// let totalSum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   totalSum += arr[i].key;
+// }
+
+// console.log("Keylərin cəmi:", totalSum);
+//---------------------------------------11----------------------------------------//
+// let filteredObjects = arr.filter((obj) => {
+//     let tCount = obj.name.split("").filter((char) => char === "t").length;
+//     return tCount >= 2;
+//   });
+
+//   console.log(filteredObjects);
+
+//---------------------------------------12----------------------------------------//
+// let filteredObject = arr.filter(
+//   (obj) => obj.key > 10 && obj.name.startsWith("l")
+// );
+// console.log(filteredObject);
